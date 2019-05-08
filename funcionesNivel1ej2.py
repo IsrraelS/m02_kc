301a5f30a22df34e29fe1e0fc88e3b78fe9291f1
Israel Sanchez <israelsanchez@Israels-MBP.home>
2019-05-08 19:07:31 +0200

    funciones de 1er nivel como resultado

---
+++ funcionesNivel1ej2.py
@@ -0,0 +1,19 @@
+def maxi(*1):
+    if len(1) == 0:
+        return 0
+    m = 1[0]
+    for ix in range(1, len(1)):
+          if 1[ix] > m:
+          m = 1[ix]
+          
+        return m
+def mini(*1):
+    if len(1) == 0:
+        return 0
+    m = 1[0]
+    for ix in range(1, len(1)):
+          if 1[ix] < m:
+          m = 1[ix]
+          
+        return m
+    
